572a7d1b64d67f45a4246c434264b1ab1e3e8dff
CatMaster <126520391+Ndnestor098@users.noreply.github.com>
2024-02-07 17:40:54 +0100

    Update sw.js
    Corrigiendo errores

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
 const CACHE_STATIC_NAME = "static-v2";
-const filesCaches = ["./index.html", "./gracias.html", "./css/style.css", "./img/present.png", "./img/aciz.png", "./img/cv.png",
-"./img/gato.png", "./img/io2.png", "./js/main.js", "./js/efectoLuz.js","/img/favicon.ico",
-"https://fonts.gstatic.com/s/opensans/v40/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTS-muw.woff2",
-"https://kit.fontawesome.com/8f34396e62.js",
-]
 
 self.addEventListener("install", (e)=>{
     const cacheStatic = caches.open(CACHE_STATIC_NAME).then(cache=>{
-        return cache.addAll(filesCaches);
+        return cache.addAll(["./index.html", "./gracias.html", "./css/style.css", "./img/present.png", "./img/aciz.png", "./img/cv.png",
+            "./img/gato.png", "./img/io2.png", "./js/main.js", "./js/efectoLuz.js","/img/favicon.ico",
+            "https://fonts.gstatic.com/s/opensans/v40/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTS-muw.woff2",
+            "https://kit.fontawesome.com/8f34396e62.js",
+        ]);
     });
 
     e.waitUntil(Promise.all([cacheStatic]));
