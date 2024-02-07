6927e47ec232fa6c82405bd15b7bde628e2db919
CatMaster <126520391+Ndnestor098@users.noreply.github.com>
2024-02-07 17:45:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE_STATIC_NAME = "static-v2";
-
 self.addEventListener("install", (e)=>{
-    const cacheStatic = caches.open(CACHE_STATIC_NAME).then(cache=>{
+    const cacheStatic = caches.open("static-v4").then(cache=>{
         return cache.addAll(["./index.html", "./gracias.html", "./css/style.css", "./img/present.png", "./img/aciz.png", "./img/cv.png",
             "./img/gato.png", "./img/io2.png", "./js/main.js", "./js/efectoLuz.js","/img/favicon.ico",
             
