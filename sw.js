32ac2b982a496229f33c1e116e81b7b7c6a8fb2a
CatMaster <126520391+Ndnestor098@users.noreply.github.com>
2024-02-07 18:03:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,15 +3,15 @@ self.addEventListener("install", (e)=>{
         return cache.addAll([
             "./index.html", "./gracias.html", "./css/style.css", 
             "./img/aciz.png", "./img/cv.png", "./js/main.js", 
-            "./img/gato.png", "./img/io2.png", "/img/favicon.ico",
-            "./js/efectoLuz.js", "./img/present.png",
+            "./img/gato.png", "./img/io2.png", 
+            "./js/efectoLuz.js", 
         ]);
     });
 
     e.waitUntil(Promise.all([cacheStatic]));
 });
 // En construccion
-
+//"/img/favicon.ico","./img/present.png",
 // self.addEventListener("fetch", async e=>{
 //     if(!navigator.onLine){
 //         e.respondWith(handleRequest(e.request))
