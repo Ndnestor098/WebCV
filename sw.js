70bf19b992d275e575710fe07964890d9c97cf1e
CatMaster <126520391+Ndnestor098@users.noreply.github.com>
2024-02-07 17:37:28 +0100

    Create sw.js
    Crear Service Worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_STATIC_NAME = "static-v2";
+const filesCaches = ["./index.html", "./gracias.html", "./css/style.css", "./img/present.png", "./img/aciz.png", "./img/cv.png",
+"./img/gato.png", "./img/io2.png", "./js/main.js", "./js/efectoLuz.js","/img/favicon.ico",
+"https://fonts.gstatic.com/s/opensans/v40/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTS-muw.woff2",
+"https://kit.fontawesome.com/8f34396e62.js",
+]
+
+self.addEventListener("install", (e)=>{
+    const cacheStatic = caches.open(CACHE_STATIC_NAME).then(cache=>{
+        return cache.addAll(filesCaches);
+    });
+
+    e.waitUntil(Promise.all([cacheStatic]));
+});
+
+
+self.addEventListener("fetch", async e=>{
+    if(!navigator.onLine){
+        e.respondWith(handleRequest(e.request))
+    }
+
+})
+
+async function handleRequest(request) {
+    const response = await caches.match(request);
+
+    if(response) return response;
+    else return request;
+}
