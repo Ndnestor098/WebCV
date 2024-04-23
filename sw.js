01297f26e799b20b6147689f86152b918136773d
CatMaster <trabajo.nestor.098@gmail.com>
2024-04-23 17:52:58 +0200

    Actualizacion de los enlaces

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 "use strict";
 const STATIC_CACHE = "cache";
 const APP_SHELL =[
-    '/',
-    '/index.html',
-    '/gracias.html',
-    '/js/ajax.js',
-    '/js/efectoLuz.js',
-    '/js/main.js',
-    '/css/style.css',
-    '/img/aciz.png',
-    '/img/cv.png',
-    '/img/gato.png',
-    '/img/io2.png',
-    '/img/translog.png',
-    '/img/scarpetoss.png',
-    '/img/favicon/favicon.ico',
-    '/img/favicon/apple-touch-icon.png',
-    '/img/favicon/android-chrome-512x512.png',
+    './',
+    './index.html',
+    './gracias.html',
+    './js/ajax.js',
+    './js/efectoLuz.js',
+    './js/main.js',
+    './css/style.css',
+    './img/aciz.png',
+    './img/cv.png',
+    './img/gato.png',
+    './img/io2.png',
+    './img/translog.png',
+    './img/scarpetoss.png',
+    './img/favicon/favicon.ico',
+    './img/favicon/apple-touch-icon.png',
+    './img/favicon/android-chrome-512x512.png',
 ] 
 
 self.addEventListener("install", function (e){
